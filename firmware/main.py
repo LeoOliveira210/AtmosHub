db7a8f2149d2cd5b19badedd42caf615d6540458
Kayk Reis <k4yk.r3is10@gmail.com>
2026-09-07 21:59:38 -0300

    Update main.py
    Organiza leituras dos sensores em variáveis globais no main.py

--- firmware/main.py
+++ firmware/main.py
@@ -18,20 +18,41 @@ rain = RainSensor(27, mm_per_pulse=0.2)
 
 ultimo_print = ticks_ms()
 
+#Sensor Temperatura (DHT22)
+dht22.measure()
+temperatura_ar = dht22.temperature()
+umidade_ar = dht22.humidity()
+
+#Sensor BMP180
+temperatura_bmp = bmp.temperature
+pressao_hpa = bmp.pressure / 100
+
+#Sensor LDR e MQ135
+luminosidade_bruta = ldr.read()
+qualidade_ar_bruta = mq135.read()
+
+#Sensor chuva_pulsos e chuva_acumulada
+chuva_pulsos = rain.pulses
+chuva_acumulada = rain.rain_mm
+
+
 while True:
     rain.update()
 
     agora = ticks_ms()
     if ticks_diff(agora, ultimo_print) >= 2000:
+
         dht22.measure()
         temperatura_ar = dht22.temperature()
         umidade_ar = dht22.humidity()
 
         temperatura_bmp = bmp.temperature
         pressao_hpa = bmp.pressure / 100
-
         luminosidade_bruta = ldr.read()
         qualidade_ar_bruta = mq135.read()
+
+        chuva_pulsos = rain.pulses
+        chuva_acumulada = rain.rain_mm
 
         print("=== ATMOSHUB ===")
         print("Temperatura (DHT22):", temperatura_ar, "C")
@@ -40,8 +61,8 @@ while True:
         print("Pressao:", pressao_hpa, "hPa")
         print("Luminosidade (bruta):", luminosidade_bruta)
         print("Qualidade do ar (bruta):", qualidade_ar_bruta)
-        print("Chuva - pulsos:", rain.pulses)
-        print("Chuva acumulada:", rain.rain_mm, "mm")
+        print("Chuva - pulsos:", chuva_pulsos)
+        print("Chuva acumulada:", chuva_acumulada, "mm")
         print("------------------------------")
 
         ultimo_print = agora
